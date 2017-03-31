808bdc6273cf6b6b5906abeffcd232a7b77aec09
KRISHNA\CHANDAN <chandan115325@gmail.com>
2017-03-31 17:24:26 +0530

    no message

--- src/session_02_java_assignment_02/AssignmentToPrintPattern.java
+++ src/session_02_java_assignment_02/AssignmentToPrintPattern.java
@@ -51,7 +51,7 @@ public class AssignmentToPrintPattern {
 	           	                       
 	            else
 	            {
-	            	System.out.print(" ");
+	            //	System.out.print(" ");
 	            }
 	         }
 	        System.out.println();
